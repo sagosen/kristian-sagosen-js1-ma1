e0c77445c9cbc7f2c8df933e48e1b03b0705cceb
Kristian Sagosen <49401442+sagosen@users.noreply.github.com>
2023-02-05 18:43:07 +0100

    Questions 1-5 answered
    Just a quick upload in case I need to leave house.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,36 @@ const cats = [
         age: 21
     }
 ];
+
+// Question 1
+
+const cat = {
+    complain : function() {
+        console.log("Meow!");
+    }
+}
+
+cat.complain();
+
+
+// Question 2
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+
+heading.classList.add("subheading");
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for (let i=0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+}
